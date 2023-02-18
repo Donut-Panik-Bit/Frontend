c40a1fdbe4e84b8d79f30408d5e0b72a29d5626c
webdev1l <samofalov.r.a@gmail.com>
2023-02-18 17:11:10 +0300

    Chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import styled from 'styled-components';
 
-import {ReactComponent as CloseButtonIcon} from '../assets/svg/basil_cross-outline.svg'
-import {ReactComponent as MicroButtonIcon} from '../assets/svg/Micro.svg'
-import {ReactComponent as SendButtonIcon} from '../assets/svg/Send.svg'
-import {ReactComponent as Granat} from '../assets/svg/Granat.svg'
-import {ReactComponent as GranatRightLeg} from '../assets/svg/Granat-right-leg.svg'
-import {ReactComponent as GranatLeftLeg} from '../assets/svg/Granat-left-leg.svg'
+import { ReactComponent as CloseButtonIcon } from '../assets/svg/basil_cross-outline.svg';
+import { ReactComponent as MicroButtonIcon } from '../assets/svg/Micro.svg';
+import { ReactComponent as SendButtonIcon } from '../assets/svg/Send.svg';
+import { ReactComponent as Granat } from '../assets/svg/Granat.svg';
+import { ReactComponent as GranatRightLeg } from '../assets/svg/Granat-right-leg.svg';
+import { ReactComponent as GranatLeftLeg } from '../assets/svg/Granat-left-leg.svg';
+import { ReactComponent as GranatLogo } from '../assets/svg/Granat-button.svg';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+import { useNavigate } from 'react-router-dom';
+import { paths } from '../utils/pages';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from 'react-speech-recognition';
+
+const GranatButton = styled.button`
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+  border-radius: 50%;
+  width: 80px;
+  height: 80px;
+  border: none;
+  outline: none;
+  background: #fde9ca;
+  box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.25);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const StyledGranatLogo = styled(GranatLogo)`
+  width: 53px;
+  height: 53px;
+`;
 
 const StyledChat = styled.div`
   font-family: 'PT Root UI', sans-serif;
   position: fixed;
   right: 50px;
   bottom: 50px;
-  width: 530px;
-  height: 675px;
+  width: 430px;
+  height: 600px;
   display: flex;
   flex-direction: column;
   border-radius: 20px !important;
   border-collapse: separate;
-  color: #2E2F33;
+  color: #2e2f33;
   box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.25);
   box-sizing: border-box;
   z-index: 2;
@@ -28,61 +57,65 @@ const StyledChat = styled.div`
 
 const StyledGranat = styled(Granat)`
   position: absolute;
-  right: 30px;
+  right: 55px;
   top: -80px;
 `;
 
 const StyledGranatRightLeg = styled(GranatRightLeg)`
   position: absolute;
-  right: 72px;
+  right: 97px;
   top: -15px;
   z-index: 3;
 `;
 
 const StyledGranatLeftLeg = styled(GranatLeftLeg)`
   position: absolute;
-  right: 35px;
+  right: 60px;
   top: -15px;
   z-index: 3;
 `;
 
 const Header = styled.header`
-  height: 80px;
-  background-color: #FAA419;
+  height: 55px;
+  background-color: #faa419;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 32px;
+  padding: 0 30px;
   border-radius: 20px 20px 0 0;
   z-index: 2;
 `;
 
 const Heading = styled.h2`
   font-weight: 700;
-  font-size: 28px;
+  font-size: 22px;
   line-height: 34px;
 `;
 
 const CloseButton = styled(CloseButtonIcon)`
   cursor: pointer;
+  width: 15px;
+  height: 15px;
 `;
 
 const Messages = styled.main`
   background-color: #fff;
   z-index: 100;
   flex-grow: 1;
-  padding: 24px;
+  padding: 15px;
+  padding-bottom: 0;
   display: flex;
   flex-direction: column;
   overflow-y: scroll;
+  height: 100%;
 `;
 
 const Message = styled.article`
-  padding: 20px;
-  max-width: 320px;
+  padding: 10px 15px;
+  max-width: 300px;
   word-break: break-word;
-  line-height: 24px;
-  margin-bottom: 20px;
+  line-height: 1.35;
+  margin-bottom: 10px;
   font-weight: 500;
 
   &:last-child {
@@ -93,33 +126,35 @@ const Message = styled.article`
 `;
 
 const IncomingMessage = styled(Message)`
-  background-color: #EAEAEF;
+  background-color: #eaeaef;
   align-self: flex-start;
   border-radius: 20px 20px 20px 0;
 `;
 
 const OutgoingMessage = styled(Message)`
-  background-color: #FAA419;
+  background-color: #c2abef;
   align-self: flex-end;
   border-radius: 20px 20px 0 20px;
 `;
 
 const Footer = styled.form`
+  background-color: #fff;
   display: flex;
   align-items: center;
-  height: 100px;
+  height: 70px;
   border-radius: 0 0 20px 20px;
-  padding: 0 12px 0 24px;
+  padding: 10px 25px 10px 15px;
 `;
 
 const MessageInput = styled.input`
+  font-family: inherit;
+  font-size: inherit;
   width: 100%;
   border: none;
   outline: none;
-  background: #F1F1F5;
+  background: #f1f1f5;
   border-radius: 20px;
   font-weight: 500;
-  font-size: 20px;
   line-height: 24px;
   padding: 12px 36px 12px 24px;
 `;
@@ -135,7 +170,7 @@ const Control = styled.div`
     width: 50%;
     height: 100%;
     position: absolute;
-    background-color: #FAA419;
+    background-color: #faa419;
     left: 50%;
     transform: translateX(-50%);
   }
@@ -149,10 +184,9 @@ const Divider = styled.div`
   transform: translate(-50%, -50%);
   width: 2px;
   height: 70%;
-  background-color: #DC762B;
+  background-color: #dc762b;
   border-radius: 20px;
 `;
-
 
 const Button = styled.button`
   border: none;
@@ -160,7 +194,7 @@ const Button = styled.button`
   width: 47px;
   height: 47px;
   border-radius: 50%;
-  background-color: #FAA419;
+  background-color: #faa419;
   z-index: 2;
   display: flex;
   justify-content: center;
@@ -173,37 +207,158 @@ const Button = styled.button`
   }
 `;
 
-const Chat = () => {
-  return (
+interface Message {
+  isIncome: boolean;
+  value: string;
+}
+
+const Chat: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [userMessage, setUserMessage] = useState<string>('');
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [ws, setWs] = useState<WebSocket | null>(null);
+
+  const userInputRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLDivElement>(null);
+
+  const navigate = useNavigate();
+
+  type IcommandOpen = {
+    command: string;
+    callback: (site: any) => void;
+  };
+
+  const commands: IcommandOpen[] = [
+    {
+      command: 'посмотреть информацию',
+      callback: () => navigate(paths.info),
+    },
+    {
+      command: 'посмотреть главную',
+      callback: () => navigate(paths.home),
+    },
+  ];
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition({ commands });
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://92.63.102.121/v1/ws/1');
+    ws.onopen = () => console.log('open');
+    ws.onmessage = (e) => {
+      setMessages((prev) => [
+        ...prev,
+        {
+          isIncome: true,
+          value: e.data,
+        },
+      ]);
+    };
+    ws.onclose = () => console.log('close');
+    setWs(ws);
+    userInputRef?.current?.focus();
+  }, [setMessages]);
+
+  useEffect(() => {
+    if (isOpen) {
+      userInputRef?.current?.focus();
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (transcript === '') {
+      setUserMessage(transcript);
+    } else {
+      setUserMessage((prev) => prev + transcript);
+    }
+  }, [transcript]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!userMessage) return;
+    if (ws) {
+      console.log('sending');
+      ws.send(userMessage);
+    }
+    setMessages((prev) => [
+      ...prev,
+      {
+        isIncome: false,
+        value: userMessage,
+      },
+    ]);
+    setUserMessage('');
+    userInputRef?.current?.focus();
+    resetTranscript();
+  };
+
+  useEffect(() => {
+    if (messageRef.current) {
+      messageRef.current.scrollTop = messageRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  const handleRecord = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if (!listening) {
+      resetTranscript();
+      SpeechRecognition.startListening({ continuous: true, language: 'ru-RU' });
+    } else {
+      SpeechRecognition.stopListening();
+    }
+  };
+
+  return isOpen ? (
     <StyledChat>
       <Header>
         <Heading>Помощник Гранат</Heading>
-        <CloseButton/>
+        <CloseButton onClick={() => setIsOpen(false)} />
       </Header>
-      <Messages>
-        <IncomingMessage>asdasdasdasdasdasdasdasdasdasdasdasdasdasd</IncomingMessage>
-        <OutgoingMessage>AasdasdaAaAasdasdaAasdasdaAasdasdaAasdasdaAasdasdaAasdasda</OutgoingMessage>
-        <OutgoingMessage>AasdasdaAasdasdaAasdasdaAasdasdaAasdsdaAasdasdaAasdasda</OutgoingMessage>
-        <IncomingMessage>asdasd</IncomingMessage>
-        <OutgoingMessage>AasdasdaAasdasdasdasdaAasdasdaAasdasdaAasdasdaAasdasda</OutgoingMessage>
-        <OutgoingMessage>AasdasdaAasdasdaAasdasdaAasdasdaAasdasdaAasdasdaAasdasdaAasdasda</OutgoingMessage>
+      <Messages ref={messageRef}>
+        {messages.map((message, index) =>
+          message.isIncome ? (
+            <IncomingMessage key={message.value + index}>
+              {message.value}
+            </IncomingMessage>
+          ) : (
+            <OutgoingMessage key={message.value + index}>
+              {message.value}
+            </OutgoingMessage>
+          )
+        )}
       </Messages>
-      <Footer>
-        <MessageInput/>
+      <Footer onSubmit={handleSubmit}>
+        <MessageInput
+          value={userMessage}
+          onChange={(e) => setUserMessage(e.target.value)}
+          ref={userInputRef}
+        />
         <Control>
-          <Button>
-            <MicroButtonIcon/>
+          <Button onClick={handleRecord}>
+            <MicroButtonIcon />
           </Button>
-          <Divider/>
-          <Button>
-            <SendButtonIcon/>
+          <Divider />
+          <Button type={'submit'}>
+            <SendButtonIcon />
           </Button>
         </Control>
       </Footer>
-      <StyledGranat/>
+      <StyledGranat />
       <StyledGranatRightLeg />
       <StyledGranatLeftLeg />
     </StyledChat>
+  ) : (
+    <GranatButton
+      onClick={() => {
+        setIsOpen(true);
+      }}
+    >
+      <StyledGranatLogo />
+    </GranatButton>
   );
 };
 
